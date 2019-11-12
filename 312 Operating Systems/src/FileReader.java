4c697763974e519cca3691b5b291b2bca7d6b9e6
nathanlieu21 <lieunm@vcu.edu>
2019-11-12 09:44:17 -0500

    Fixed file reading input

--- 312 Operating Systems/src/FileReader.java
+++ 312 Operating Systems/src/FileReader.java
@@ -12,15 +12,16 @@ public class FileReader {
 		int counter =0;
 		String name = "";
 		int filecounter=0;
-		int x;
+		String x="";
 		ArrayList<Process> allfiles = new ArrayList<Process>();
 		System.out.println("How many files do you have");
-		x=in.nextInt();
-		while(filecounter<x) // number of files
+		x=in.nextLine();
+		while(filecounter<Integer.parseInt(x)) // number of files
 		{
 			System.out.println("enter file name");
 			filename=in.nextLine();
-			File file = new File(filename);
+			filecounter++;
+			File file = new File("C:\\Users\\thebe\\git\\Nathan_Lieu_CMSC312_2019\\.git\\312 Operating Systems\\" + filename);
 			Scanner s = new Scanner(file);
 			//Scanner s = new Scanner(new File(filename));
 			ArrayList<String> list = new ArrayList<String>();
@@ -33,27 +34,31 @@ public class FileReader {
 				{
 			
 					name= list.get(i + 1);
-					list.remove(i);// removes name 
-					list.remove(i+1);// removes file name
+					System.out.println(name);
+					//list.remove(i);// removes name 
+					//list.remove(i+1);// removes file name
 				}
-				if(list.get(i).equals("Runtime:"))
+				if(list.get(i).equals("runtime:"))
 				{
 					runtime=Integer.parseInt(list.get(i+1));
-					list.remove(i); //removes runtime
-					list.remove(i+1); //removes runtime value
+					System.out.println(runtime);
+					//list.remove(i); //removes runtime
+					//list.remove(i+1); //removes runtime value
 				}
 				if(list.get(i).equals("Memory:"))
 				{
 					memory=Integer.parseInt(list.get(i+1));
-					list.remove(i);//removes memory
-					list.remove(i+1); //removes memory value
+					System.out.println(memory);
+					//list.remove(i);//removes memory
+					//list.remove(i+1); //removes memory value
 				}
 			}
 			counter++;//pid starts from 1 and goes up
 			Process p = new Process(name,counter,runtime,memory,list);
 			allfiles.add(p);
+			
 			s.close();
-		
+			
 		}	
 	}// default as driver class
 }
